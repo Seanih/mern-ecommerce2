a18989afc542a63caeec1cdf4698d74b91f9500a
Sean Anih <sean.anih@gmail.com>
2021-01-05 19:47:50 -0500

    Added react-bootstrap and font-awesome. Started making components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 function App() {
 	return (
 		<>
-			<h1>Cold Commerce</h1>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome to Cold Commerce</h1>
+				</Container>
+			</main>
+			<Footer />
 		</>
 	);
 }
